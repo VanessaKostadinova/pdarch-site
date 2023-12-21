002f0f550f0074f0078c0f14fd0071bcbacb13a9
Vanessa Kostadinova <vanessa_g_k@hotmail.com>
2023-12-21 20:42:32 +0000

    added a main contents component for text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Navbar from "./navbar"
-import Carousel from "./carousel"
+import Carousel from "./images/carousel"
+import MainContent from './content/main-page'
 
 export default function Home() {
   return (
     <main className="">
       <Navbar />
       <Carousel />
+      <MainContent />
     </main>
   )
 }
