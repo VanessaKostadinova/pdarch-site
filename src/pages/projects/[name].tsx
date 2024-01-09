d8f6e92ee8218da40b93d4ad20ae5edbd7d2940c
Vanessa Kostadinova <vanessa_g_k@hotmail.com>
2024-01-09 14:10:04 +0000

    hooked up the project files dynamically to a list of json files. enabled static site generation

--- src/pages/projects/[name].tsx
+++ src/pages/projects/[name].tsx
@@ -1,11 +1,38 @@
 import { useRouter } from 'next/router'
+import { promises as fs } from 'fs'
+import path from 'path'
 
-export default function Page() {
+type PropContent = {
+  title: string;
+}
+
+export default function Page( props: PropContent) {
     const router = useRouter();
+
     return (
       <main>
-        <p>Post: {router.query.name}</p>
+        <p>Post: { props.title }</p>
       </main>
     )
   }
-  
+
+export const getStaticPaths = (async() => {
+  const postsDirectory = path.join(process.cwd(), 'src', 'content', 'projects');
+  const filenames = await fs.readdir(postsDirectory);
+
+  const paths = filenames.map(filename => ({
+    params: { name: filename.replace('.json', '') },
+  }));
+
+  return {
+    paths, fallback: false
+  }
+})
+
+export const getStaticProps = (async ({ params } : {params: {name: string}}) => {
+  const postsDirectory = path.join(process.cwd(), 'src', 'content', 'projects', `${params.name}.json`);
+  const fileContents = await fs.readFile(postsDirectory, 'utf8');
+  const data = JSON.parse(fileContents);
+
+  return { props: { ...data } };
+})
