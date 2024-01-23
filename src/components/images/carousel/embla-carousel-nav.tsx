365e4a068566062296c728b32e3443a30ef8a152
Vanessa Kostadinova <vanessa_g_k@hotmail.com>
2024-01-23 23:19:00 +0000

    Added image dot selection to the embla carousel

---
+++ src/components/images/carousel/embla-carousel-nav.tsx
@@ -0,0 +1,74 @@
+import React, {
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useState
+} from 'react'
+import { EmblaCarouselType } from 'embla-carousel'
+
+type UseDotButtonType = {
+  selectedIndex: number
+  scrollSnaps: number[]
+  onDotButtonClick: (index: number) => void
+}
+
+export const useDotButton = (
+  emblaApi: EmblaCarouselType | undefined
+): UseDotButtonType => {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([])
+
+  const onDotButtonClick = useCallback(
+    (index: number) => {
+      if (!emblaApi) return
+      emblaApi.scrollTo(index)
+    },
+    [emblaApi]
+  )
+
+  const onInit = useCallback((emblaApi: EmblaCarouselType) => {
+    setScrollSnaps(emblaApi.scrollSnapList())
+  }, [])
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap())
+  }, [])
+
+  useEffect(() => {
+    if (!emblaApi) return
+
+    onInit(emblaApi)
+    onSelect(emblaApi)
+    emblaApi.on('reInit', onInit)
+    emblaApi.on('reInit', onSelect)
+    emblaApi.on('select', onSelect)
+  }, [emblaApi, onInit, onSelect])
+
+  return {
+    selectedIndex,
+    scrollSnaps,
+    onDotButtonClick
+  }
+}
+
+type DotButtonProps = PropsWithChildren<
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > & {
+    imgSrc: string;
+    imgAlt: string;
+    imgClassName: string;
+  }
+>;
+
+export function DotButton(props: DotButtonProps) {
+  const { imgSrc, imgAlt, imgClassName, ...restProps } = props;
+
+  return (
+    <button type="button" {...restProps}>
+      {/* Pass the image-related props to the img element */}
+      <img src={imgSrc} className={imgClassName} alt={imgAlt} />
+    </button>
+  );
+}
--- src/components/images/carousel/embla-carousel.tsx
+++ src/components/images/carousel/embla-carousel.tsx
@@ -2,26 +2,46 @@
 import React, { useEffect } from 'react';
 import useEmblaCarousel from 'embla-carousel-react'
 import "./embla-carousel.css"
+import { DotButton, useDotButton } from './embla-carousel-nav'
 
 type Image = {
     src: string,
     alt: string
 };
 
-export const EmblaCarousel = (props: {images: Image[]}) => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+export const EmblaCarousel = (props: { images: Image[] }) => {
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
 
-  return (
-    <div className="embla">
-        <div className="embla__viewport" ref={emblaRef}>
-            <div className="embla__container">
-                {props.images.map((image, index) => (
-                <div className="embla__slide" key={index}>
-                    <img className="embla__slide__img" src={image.src} alt={image.alt} />
+    const { selectedIndex, scrollSnaps, onDotButtonClick } =
+        useDotButton(emblaApi)
+
+    return (
+        <>
+            <div className="embla">
+                <div className="embla__viewport" ref={emblaRef}>
+                    <div className="embla__container">
+                        {props.images.map((image, index) => (
+                            <div className="embla__slide" key={index}>
+                                <img className="embla__slide__img" loading={index==0 ? 'eager': 'lazy'} src={image.src} alt={image.alt} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
+            </div>
+            <div className="w-full md:w-4/5 lg:w-2/3 xl:w-1/2 flex flex-row flex-wrap gap-[1rem] mt-[1rem] justify-center sm:justify-start items-center px-4 md:px-0">
+                {props.images.map((image, index) => (
+                    <DotButton
+                        key={index}
+                        imgSrc={image.src}
+                        imgAlt={image.alt}
+                        imgClassName={`zoomed-image embla__img 
+                            ${index === selectedIndex ? 'border-white border-2 rounded-sm' : ''}
+                        `}
+                        onClick={() => onDotButtonClick(index)}
+                        className="embla__dot"
+                    />
                 ))}
             </div>
-        </div>
-    </div>
-  );
+        </>
+    );
 };
